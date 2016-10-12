2128ab6871a16cc6055432803032ec7608178314
Alan Nicholson <alan.nicco@gmail.com>
2016-10-12 15:15:56 +0100

    Add files via upload

--- wiiSc.py
+++ wiiSc.py
@@ -15,7 +15,8 @@ while 0==0:
     table = soup.find('table')      
     tr = table.find_all('tr')
     names = []
-    fCode = []
+    points = []
+    fCodes = []
     for td in tr:
         try:
              n=td.contents[13].text #Grabs names from Cells
@@ -25,16 +26,27 @@ while 0==0:
     names = [ n for n in names if n != 'Mii name']
     for n in names:
         if n.startswith('1. '): # If 2 players on same Wii
-                n.replace('1. ','')
-                n.split('2. ') # Creates list of the two names
+                n = n.replace('1. ','')
+                n.replace('2. ',' ')
+                n = n.split() # Creates list of the two names
+                #Can't append list in a loop.......
     for td in tr:
         try:
-             n=td.contents[1].text #Grabs FriendCodes from Cells
-             fCode.append(n)
+             v=td.contents[9].text #Grabs versus points from Cells
+             points.append(v)
         except:
             pass
-    fCode = [f for f in fCode if f != 'friend code']
-    mkDic = dict(zip(fCode,names))
+    points = [ v for v in points if v != 'versuspoints']
+       
+    for td in tr:
+        try:
+             f=td.contents[1].text #Grabs FriendCodes from Cells
+             fCodes.append(f)
+        except:
+            pass
+    fCodes = [f for f in fCodes if f != 'friend code']    
+    mkDic = dict(zip(fCodes,names))
+    mkList = list(zip(fCodes,names,points))
     #print(mkDic)
     for key, value in mkDic.items():
         if key == '2922-2443-6911': #2922-2443-6911 - fartface
@@ -46,17 +58,17 @@ while 0==0:
         elif key == '5284-6991-7807': #5284-6991-7807 - Sean
             messagebox.showinfo("Friend Found!","It's "+value)
             sys.exit()
-        elif key == '3526-1167-6604': #3526-1167-6604 - Δ¡ckΓ◎τ
-            messagebox.showinfo("Friend Found!","It's "+value)
-            sys.exit()
+        #elif key == '3526-1167-6604': #3526-1167-6604 - Δ¡ckΓ◎τ
+        #    messagebox.showinfo("Friend Found!","It's "+value)
+        #    sys.exit()
         else:
             pass   
     if p != '':
         for key, value in mkDic.items():
             if p in value:
                 print(key) # Prints Friend-Code
-                #messagebox.showinfo("Player Found!","It's "+value)
-                #print(mkDic)
+                messagebox.showinfo("Player Found!","It's "+value)
+                #print(mkList)
                 sys.exit()
                 
             else:
@@ -64,3 +76,4 @@ while 0==0:
        
     #print('1')
     time.sleep(int(t))
+
