be8a64703408e625efc19c2a0fd0eea4eb233da2
A_N <alan.nicco@gmail.com>
2016-10-14 14:10:19 +0100

    Add files via upload

--- wiiScCount.py
+++ wiiScCount.py
@@ -2,19 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import sys
-
+from tkinter import *
 
 urlMK = 'https://wiimmfi.de/mkw'
+global mkVsCount
+root = Tk()
+root.title("Online Players")
 
-t = input('Refresh Rate: ')
-
-while True:
+def mkCount():
     r = requests.get(urlMK)
     soup = BeautifulSoup(r.content, "lxml")
     table = soup.find('table')      
     tr = table.find_all('tr')
     region = []
-
     for td in tr:
         try:
              r=td.contents[5].text
@@ -22,11 +22,13 @@ while True:
         except:
             pass
         
-    region = [r for r in region if r != 'region']
+    region = [r for r in region if r != 'region']  
+    mkVsCount = len([a for a in region if 'room' not in a]) # Only WW and Cont
+    #print(mkVsCount)
+    pCount.configure(text = mkVsCount)
+    root.after(5000,mkCount) # ReRegistered Callback inside Itself
 
-    mkVsCount = len([a for a in region if 'room' not in a])
-
-    print(mkVsCount)
-
-    time.sleep(int(t))
-
+pCount = Label(root, font = "Helvetica 44 bold",width='6')
+pCount.pack()
+root.after(5000,mkCount)
+root.mainloop()
