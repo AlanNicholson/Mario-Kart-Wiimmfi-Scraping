3a846316b6e1059156d8eb2c6fde60e9799cb6f4
A_N <alan.nicco@gmail.com>
2016-10-15 12:40:38 +0100

    Add files via upload
    Creates csv of player numbers over time.

---
+++ wiiScCSV.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import time
+import csv
+import sys
+from tkinter import *
+
+urlMK = 'https://wiimmfi.de/mkw'
+PNL = []
+format = "%H:%M:%S %a %d %b"
+with open('Wiimmfi_Numbers.csv', 'w', encoding='utf-8') as WN:
+    w = csv.writer(WN, dialect='excel')
+    header = ['Vs. Players', 'Room Players', 'Time']
+    w.writerow(header)
+
+
+def mkCount():
+    while True:
+        r = requests.get(urlMK)
+        soup = BeautifulSoup(r.content, "lxml")
+        table = soup.find('table')
+        tr = table.find_all('tr')
+        region = []
+        for td in tr:
+            try:
+                r = td.contents[5].text
+                region.append(r)
+            except:
+                pass
+
+        region = [r for r in region if r != 'region']
+        mkRCount = len([a for a in region if 'room' in a])  # Roomies
+        mkVsCount = len([a for a in region if 'room' not in a])  # Only WW and Cont
+        now = datetime.datetime.now()
+        Now = now.strftime(format)
+        snapshot = [mkVsCount,mkRCount,Now]
+        with open('Wiimmfi_Numbers.csv', 'a', encoding='utf-8') as WN:
+            w = csv.writer(WN, dialect='excel')
+            w.writerow(snapshot)
+        PNL.append(snapshot)
+        print(snapshot)
+
+        time.sleep(5)
+mkCount()
