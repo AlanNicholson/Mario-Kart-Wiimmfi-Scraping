1d619405b2b862a76b6fde6f5f3ea422cabaf0f6
A_N <alan.nicco@gmail.com>
2016-10-17 14:45:58 +0100

    Add files via upload
    Plots csv data from wiiSc

---
+++ wiiScPlot.py
@@ -0,0 +1,24 @@
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+fig = plt.figure()
+
+rect = fig.patch
+rect.set_facecolor('white')
+
+df = pd.read_csv('Wiimmfi_Numbers.15.16.Oct(Formatted).csv')
+
+graph1 = fig.add_subplot(1, 1, 1, axisbg='white')
+
+graph1.plot()
+pd.DataFrame.plot(df)
+
+xAx = list(range(0, len(df['Time'])))
+plt.xticks(xAx, df['Time'])
+plt.plot(df['Vs. Players'])
+plt.plot(df['Room Players'])
+
+plt.axis('auto')
+plt.show()
