250b8d30072fc0d9f1f85e1af1f21432a7eab32e
A_N <alan.nicco@gmail.com>
2016-10-18 18:40:34 +0100

    Add files via upload

--- wiiScPlot.py
+++ wiiScPlot.py
@@ -14,15 +14,22 @@ graph1 = fig.add_subplot(1, 1, 1, axisbg='white')
 
 graph1.plot()
 pd.DataFrame.plot(df)
+xAx = list(range(0, len(df['Time']), 80))
 
-xAx = list(range(0, len(df['Time'])))
-plt.xticks(xAx, df['Time'])
+fTime = df['Time'][::80]
+gTime = []
+for i in fTime:
+    a = i[:-14]
+    gTime.append(a)
+
+plt.xticks(xAx, gTime)
 plt.title('Wiimmfi MarioKart', y=1.08)
 plt.xlabel('Time')
 plt.ylabel('No. of Players Online')
 plt.plot(df['Vs. Players'], color='blue')
 plt.plot(df['Room Players'], color='red')
 plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
+plt.rcParams.update({'font.size': 10})
 
-plt.axis('auto')
+plt.grid()
 plt.show()
